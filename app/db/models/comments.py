90dde067856a202485239944f9aa4109b3313e5d
Andrey-Khohlov <airsofter@ya.ru>
2025-01-18 01:02:40 +0300

    back add relationship, FK, TYPE_CHECKING

--- app/db/models/comments.py
+++ app/db/models/comments.py
@@ -5,15 +5,20 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.models import Model, intpk, created_at, updated_at
 
+# Ленивый импорт для UsersAddModel
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.db.models.users import UsersAddModel
+
 
 class CommentsAddModel(Model):
     __tablename__: str = "comments"
     id: Mapped[intpk]
     product_id: Mapped[int]
-    user_id: Mapped[int] #= mapped_column(ForeignKey("UsersAddModel.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     content: Mapped[str]
     parent_id: Mapped[Optional[int]]
     review_id: Mapped[int]
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
-    # user: Mapped["UsersAddModel"] = relationship("UsersAddModel", back_populates="comment")
+    user: Mapped["UsersAddModel"] = relationship("UsersAddModel", back_populates="comment")
--- app/db/models/users.py
+++ app/db/models/users.py
@@ -2,6 +2,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.models import Model, str_256, intpk, created_at, updated_at
 
+# Ленивый импорт для CommentsAddModel
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.db.models.comments import CommentsAddModel
+
 
 class UsersAddModel(Model):
     __tablename__: str = "users"
@@ -11,4 +16,4 @@ class UsersAddModel(Model):
     password: Mapped[str]
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
-    # comment: Mapped[list["CommentsAddModel"]] = relationship( "CommentsAddModel", back_populates="user", lazy="dynamic", cascade="all, delete, delete-orphan" )
+    comment: Mapped[list["CommentsAddModel"]] = relationship( "CommentsAddModel", back_populates="user", lazy="dynamic", cascade="all, delete, delete-orphan" )
