6f194e0d8576e2173fdd86ce55f3b74bae2ed64f
Andrey-Khohlov <airsofter@ya.ru>
2025-03-08 02:22:38 +0300

    back> накатил таки Initial migration Add 3 columns to coffees

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,6 @@ from app.config import settings
 config = context.config
 # Устанавливаем URL базы данных из настроек
 config.set_main_option('sqlalchemy.url', settings.DATABASE_URL_psycopg)
-# TODO config.set_main_option('sqlalchemy.url', 'postgresql+psycopg://postgres:relfhtim@localhost:5432/tmp')
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
